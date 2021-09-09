9a060cad42bcd37e23bb3e042f44d1c1f07fc1e9
정재호 <jaho901@naver.com>
2021-09-09 10:43:56 +0900

    studying

--- study/greedy_1/19539.py
+++ study/greedy_1/19539.py
@@ -3,25 +3,16 @@ sys.stdin = open('19539.txt')
 
 N = int(input())
 trees = list(map(int, input().split()))
-print(trees)
-while True:
-    maximum = max(trees)
-    minimum = min(trees)
-    max_idx = trees.index(maximum)
-    min_idx = trees.index(minimum)
-    if minimum*2 > maximum:
-        n = maximum // 2
-        trees[max_idx] = trees[max_idx] - 2*n
-        trees[min_idx] = trees[min_idx] - n
+length = N
+total = sum(trees)
+turn = total // 3
+if total % 3 != 0:
+    print('NO')
+
+else:
+    for i in trees:
+        turn -= (i//2)
+    if turn > 0:
+        print('NO')
     else:
-        n = minimum
-        trees[max_idx] = trees[max_idx] - 2*n
-        trees[min_idx] = trees[min_idx] - n
-    
-    if trees.count(0) + trees.count(1) + trees.count(2) == N:
-        if trees.count(0) == N:
-            print('YES')
-            break
-        else:
-            print('NO')
-            break
+        print('YES')
